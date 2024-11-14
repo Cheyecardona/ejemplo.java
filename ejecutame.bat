d45984aa3135abc5e0ddea510f6ee7acb9e29ccb
Yonoveleta <thetruevayne@gmail.com>
2024-11-14 19:50:47 +0100

    otro regalo

---
+++ ejecutame.bat
@@ -0,0 +1,18 @@
+@echo off
+:: Mostrar mensaje de "hackeo"
+echo Atencion, Estas a punto de ser hackeado...
+
+:: Esperar 3 segundos para generar suspenso
+timeout /t 3 /nobreak >nul
+
+:: Mostrar mensaje final indicando que es una broma
+echo Ya no puedes hacer nada
+
+:: Esperar 3 segundos para generar suspenso
+timeout /t 3 /nobreak >nul
+
+:: Abrir el navegador y buscar "trollface" en Google
+start https://www.youtube.com/watch?v=8kQZHYbZkLs
+
+:: Terminar el script
+exit
